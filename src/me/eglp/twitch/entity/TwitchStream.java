e131a179f9ec4d4f79a7d8dee9507612c9b5c385
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2024-05-01 18:10:23 +0200

    Update MrCore

--- src/me/eglp/twitch/entity/TwitchStream.java
+++ src/me/eglp/twitch/entity/TwitchStream.java
@@ -17,61 +17,61 @@ public class TwitchStream {
 	private long viewerCount;
 	private Instant startedAt;
 	private String language, thumbnailURL;
-	
+
 	public TwitchStream(TwitchAPI twitch, JSONObject raw) {
 		this.twitch = twitch;
 		this.id = raw.getString("id");
 		this.userID = raw.getString("user_id");
 		this.gameID = raw.getString("game_id");
-		this.communityIDs = raw.has("community_ids") ? Complex.castList(raw.getJSONArray("community_ids"), String.class).get() : Collections.emptyList();
+		this.communityIDs = raw.has("community_ids") ? Complex.castList(raw.getJSONArray("community_ids").toList(), String.class).get() : Collections.emptyList();
 		this.title = raw.getString("title");
 		this.viewerCount = raw.getLong("viewer_count");
 		this.startedAt = Instant.parse(raw.getString("started_at"));
 		this.language = raw.getString("language");
 		this.thumbnailURL = raw.getString("thumbnail_url");
 	}
-	
+
 	public TwitchAPI getTwitch() {
 		return twitch;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getUserID() {
 		return userID;
 	}
-	
+
 	public String getGameID() {
 		return gameID;
 	}
-	
+
 	public TwitchGame getGame() {
 		if(gameID == null || gameID.isEmpty()) return null;
 		return twitch.getGameByID(gameID);
 	}
-	
+
 	public List<String> getCommunityIDs() {
 		return communityIDs;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public long getViewerCount() {
 		return viewerCount;
 	}
-	
+
 	public Instant getStartedAt() {
 		return startedAt;
 	}
-	
+
 	public String getLanguage() {
 		return language;
 	}
-	
+
 	public String getThumbnailURL(int width, int height) {
 		return thumbnailURL.replace("{width}", String.valueOf(width)).replace("{height}", String.valueOf(height));
 	}
